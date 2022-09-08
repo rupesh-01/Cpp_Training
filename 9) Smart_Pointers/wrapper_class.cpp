df518614a5ef82245d325d981aa1247c02763d00
Rupesh Kumar Srivastava <63953972+rupesh-01@users.noreply.github.com>
2022-09-08 12:43:20 +0530

    Update wrapper_class.cpp

--- 9) Smart_Pointers/wrapper_class.cpp
+++ 9) Smart_Pointers/wrapper_class.cpp
@@ -1,1 +1,27 @@
-//
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+class smartPtr
+{
+
+    
+
+public:
+    T *pointer;
+    explicit smartPtr(T *p = nullptr) { pointer = p; }
+    ~smartPtr() { delete pointer; }
+    T& operator*() { return *pointer; }
+};
+
+int main()
+{
+
+    // float *p = new float(3.142);
+    // smartPtr<float> smart = smartPtr(p);
+    // cout << *smart << endl;
+    char *p = new char('c');
+    smartPtr<char> smart = smartPtr(p);
+    cout << *smart << endl;
+}
