9415c9cc8afa99f07cb343a0220bbbdcbafc0307
Rupesh Kumar Srivastava <63953972+rupesh-01@users.noreply.github.com>
2022-09-08 12:52:47 +0530

    Create lambda.cpp

---
+++ 15) Multithreading/lambda.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ( ) {
+   auto add = [] ( auto x , auto y ) { return x + y ; } ;
+    int a = 1 , b = 2 ;
+   string str1 = "Maple" , str2 = " Rupesh " ;
+   cout << add ( a , b ) << endl ;
+    cout << add(str1 , str2 ) << endl;
+    return 0 ;
+}
